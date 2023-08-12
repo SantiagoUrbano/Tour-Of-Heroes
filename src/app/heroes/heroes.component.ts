53decc99640c0730666de810f0517e176141a063
Santiago Urbano <m.urbanod@uniandes.edu.co>
2023-08-12 02:13:29 -0500

    Creacion de la interfaz e implementación de 5 atributos

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: string = "Windstrom"
+  hero: Hero = {
+    id: 1,
+    name: "Windstrom",
+    power: "87%",
+    superPower: "He drop fire",
+    color: "Black and yellow"
+  }
 }
