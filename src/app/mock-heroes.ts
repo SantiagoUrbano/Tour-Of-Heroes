e416da4e1fb10828b716781986dbb8d8c66759f3
Santiago Urbano <m.urbanod@uniandes.edu.co>
2023-08-15 20:05:49 -0500

    Ajustar estilo a lista de heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./heroes/hero";
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Otalvaro. ', power: '67', superPower: "Lanza hielo", color: "Azul claro" },
+    { id: 13, name: 'Mr Bruselas. ' , power: '78', superPower: "Lanza fuego", color: "Azul oscura" },
+    { id: 14, name: 'Oliver. ', power: '34', superPower: "Lanza naranjas", color: "Azul verdoso" },
+    { id: 15, name: 'Naruto. ', power: '23', superPower: "Lanza shurikens", color: "Amarrillo" },
+    { id: 16, name: 'Annie. ', power: '75', superPower: "Lanza un oso", color: "Rojo" },
+    { id: 17, name: 'Olaf. ', power: '99', superPower: "Lanza una hacha", color: "Cafe" },
+    { id: 18, name: 'LeBlanc. ', power: '76', superPower: "Lanza hechizos", color: "Morado" },
+    { id: 19, name: 'Nami. ', power: '97', superPower: "Lanza agua", color: "Azul aguamarina" },
+    { id: 20, name: 'Phanteon. ', power: '89', superPower: "Lanza una lanza", color: "Piel" }
+
+]
